1235cea6950e3f12f9ceb47bbce61fc7cd9380e4
Gabriel <gabrieltroop27@gmail.com>
2024-12-21 17:56:48 -0300

    firstcommit

---
+++ JS/anotacoesAula1.js
@@ -0,0 +1,4 @@
+// npm i -g {package} "-g" instala no sistema inteiramente, e nao apenas no projeto
+// npm i {package} escopo de projeto
+// npx {package} executa pacote
+// npm remove {package}, npm remove -g {package}
